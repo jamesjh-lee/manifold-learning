ee6205330c8b4066a886b5d7fb320bdc1c8acafa
Jonghoon Lee <93747285+jamesjh-lee@users.noreply.github.com>
2022-03-07 18:37:18 +0900

    Update manifold_learning.py

--- manifold_learning.py
+++ manifold_learning.py
@@ -5,34 +5,13 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn import manifold, datasets
 from sklearn.decomposition import PCA
 from autoencoders import AE, VAE
+import sys
 
 n_components = 2
 n_neighbors = 10
 n_points = 1000
 
-def plot_manifold(X, methods, color):
-  fig = plt.figure(figsize=(30, 10))
-  fig.suptitle("Manifold Learning with %i points, %i neighbors"
-               % (n_points, n_neighbors), fontsize=36)
-
-  # plot X(s-curve)
-  ax = fig.add_subplot(271, projection='3d')
-  ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=plt.cm.Spectral)
-  ax.view_init(4, -72)
-  for i, (label, method) in enumerate(methods.items()):
-    t0 = time.time()
-    if label == 'AE' or label == 'VAE':
-      Y = method.fit_transform(X, shuffle=True, epochs=15, batch_size=100, verbose=0)
-    else:
-      Y = method.fit_transform(X)
-    print("%s: %.2g sec" % (label, time.time() - t0))
-    ax = fig.add_subplot(2, 7, 2 + i + (i > 4))
-    ax.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
-    ax.set_title("%s (%.2g sec)" % (label, time.time() - t0))
-  fig.tight_layout(pad=3.0)
-  plt.savefig('./plots/manifold_' + str(round(time.time())) + '.png')
-
-if __name__ == '__main__':
+def plot_s_curve():
   print('make s-curve dataset: (1000,3)')
   X, color = datasets.make_s_curve(n_points, random_state=0)
   
@@ -57,5 +36,34 @@ if __name__ == '__main__':
   methods['AE'] = AE(n_components, X.shape[-1])
   methods['VAE'] = VAE(n_components, X.shape[-1])
   
-  plot_manifold(X, methods, color)
+  fig = plt.figure(figsize=(30, 10))
+  fig.suptitle("Manifold Learning with %i points, %i neighbors"
+               % (n_points, n_neighbors), fontsize=36)
+
+  # plot X(s-curve)
+  ax = fig.add_subplot(271, projection='3d')
+  ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=plt.cm.Spectral)
+  ax.view_init(4, -72)
+  for i, (label, method) in enumerate(methods.items()):
+    t0 = time.time()
+    if label == 'AE' or label == 'VAE':
+      Y = method.fit_transform(X, shuffle=True, epochs=15, batch_size=100, verbose=0)
+    else:
+      Y = method.fit_transform(X)
+    print("%s: %.2g sec" % (label, time.time() - t0))
+    ax = fig.add_subplot(2, 7, 2 + i + (i > 4))
+    ax.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+    ax.set_title("%s (%.2g sec)" % (label, time.time() - t0))
+  fig.tight_layout(pad=3.0)
+  plt.savefig('./plots/manifold_' + str(round(time.time())) + '.png')
+
+if __name__ == '__main__':
+  if len(sys.args) != 2:
+    print('usage: python3 %s <s_curve or mnist>' % (sys.args[0]))
+    sys.exit()
   
+  print('start plotting manifold of %s' % (sys.args[1]))
+  if sys.args[1] == 'mnist':
+    plot_mnist()
+  else:
+    plot_s_curve()
