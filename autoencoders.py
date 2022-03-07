937fd5b3239b9b35589e37b5054a82f3a34a8917
Jonghoon Lee <93747285+jamesjh-lee@users.noreply.github.com>
2022-03-07 16:17:23 +0900

    Update autoencoders.py

--- autoencoders.py
+++ autoencoders.py
@@ -11,40 +11,39 @@ class AE:
     def __init__(self, n_components, origin_dim):
         self.latent_dim = n_components
         self.origin_dim = origin_dim
-        self.model = None
-        self.encoder = None
-        self.decoder = None
+        self.inputs = Input(shape=(origin_dim,))
     
-    def build_net(self):
-        # encoder
-        e_in = Input(shape=(self.origin_dim,))
-        hidden = Dense(256, activation='relu')(e_in)
+    def _build_net(self):
+        self.encoder = self._encoder()
+        self.decoder = self._decoder()
+        outputs = self.decoder(self.encoder(self.inputs))
+        self.model = Model(inputs=self.inputs, outputs=outputs, name='AE')
+        
+    def _encoder(self):
+        hidden = Dense(256, activation='relu')(self.inputs)
         hidden = Dense(128, activation='relu')(hidden)
         hidden = Dense(64, activation='relu')(hidden)
         e_out = Dense(self.latent_dim, activation='relu')(hidden)
-        self.encoder = Model(inputs=e_in, outputs=e_out, name='encoder')
+        return Model(inputs=self.inputs, outputs=e_out, name='encoder')
 
+    def _decoder(self):
         # decoder
         d_in = Input(shape=(self.latent_dim,))
         hidden = Dense(64, activation='relu')(d_in)
         hidden = Dense(128, activation='relu')(hidden)
         hidden = Dense(256, activation='relu')(hidden)
         d_out = Dense(self.origin_dim, activation='sigmoid')(hidden)
-        self.decoder = Model(inputs=d_in, outputs=d_out, name='decoder')
-        
-        # AE
-        outputs = self.decoder(self.encoder(e_in))
-        self.model = Model(inputs=e_in, outputs=outputs, name='AE')
+        return Model(inputs=d_in, outputs=d_out, name='decoder')
 
     def fit_transform(self, x, **kwargs):
-        self.build_net()
-        self.model.compile(optimizer=Adam(0.00001), loss='mae')
-        hist = self.fit(x)
+        self._build_net()
+        self.model.compile(optimizer=Adam(0.0001), loss='mae')
+        hist = self.fit(x, **kwargs)
         print(hist.history['loss'][-1])
-        return self.encoder.predict(x, **kwargs)
+        return self.transform(x)
     
     def fit(self, x, **kwargs):
-      self.model.fit(x, x, **kwargs)
+      return self.model.fit(x, x, **kwargs)
      
     def transform(self, x):
       return self.encoder.predict(x)
